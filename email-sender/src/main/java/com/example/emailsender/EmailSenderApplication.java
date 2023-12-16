36c8e890de9bf2362333ca2123f9bc5609f78b08
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-16 09:07:36 +0530

    Update package

--- email-sender/src/main/java/com/example/emailsender/EmailSenderApplication.java
+++ email-sender/src/main/java/com/example/emailsender/EmailSenderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmailSenderApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmailSenderApplication.class, args);
 	}
 
